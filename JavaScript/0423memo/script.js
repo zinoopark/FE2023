b0d9d5f7555b4a72edf411793ddfceaf56f6e683
oreozzang <93peace@naver.com>
2023-04-27 01:36:50 +0900

    add: moveMemo

--- JavaScript/0423memo/script.js
+++ JavaScript/0423memo/script.js
@@ -1,9 +1,13 @@
 const memoContainer = document.querySelector(".memoContainer"); //포스트잇 공간
+
 const title = document.querySelector("#title"); //제목
+
 const content = document.querySelector("#content"); //내용
+
 let memoArray = localStorage.getItem("memoArray")
   ? JSON.parse(localStorage.getItem("memoArray"))
   : []; //배열
+
 memoArray = memoArray ?? [];
 render();
 
@@ -21,11 +25,6 @@ function createMemo(item) {
   const h2_title = document.createElement("h2"); // 질문 h2태그 생성
   const p_content = document.createElement("p"); // 답변 p태그 생성
   const memoTime = document.createElement("p"); // 날짜 표기
-
-  const time = new Date();
-  const year = time.getFullYear();
-  const month = String(time.getMonth() + 1).padStart(2, "0"); // getMonth()는 0부터 시작하므로 1을 더해줍니다.
-  const day = String(time.getDate()).padStart(2, "0");
 
   memoId.className = "a11y-hidden";
   memoTime.className = "memoTime";
@@ -49,8 +48,8 @@ function createMemo(item) {
   list.classList.add(item.leng); // p는 보여주는 번호이기 때문에 index[0]에 1을 더해줬고, 이것은 실제 id의 번호 속성
   delMemoBtn.setAttribute("style", "");
   delMemoBtn.setAttribute("onclick", "delMemo()");
+  memoTime.textContent = item.date;
   // delMemoBtn.textContent = "삭제";
-  memoTime.textContent = `${year}/${month}/${day}`; //시간 표기
 
   // const flexDiv = document.createElement("div");
   // flexDiv.setAttribute(
@@ -72,7 +71,62 @@ function createMemo(item) {
   const randomY = parseInt(
     Math.random() * (window.innerHeight - list.offsetHeight)
   );
-  list.style.transform = `translate(${randomX}px, ${randomY}px)`;
+
+  list.style.top = randomY + "px";
+  list.style.left = randomX + "px";
+
+  list.setAttribute("onmousedown", "moveMemo()");
+  list.ondragstart = function () {
+    return false;
+  };
+
+  console.dir(list);
+}
+
+function moveMemo() {
+  if (event.target.localName === "button") return;
+  console.log(event);
+
+  const memo = event.target.closest("li");
+  if (!memo) return;
+
+  let shiftX = event.clientX - memo.getBoundingClientRect().left;
+  let shiftY = event.clientY - memo.getBoundingClientRect().top;
+
+  // 현재 위치한 부모에서 body로 직접 이동하여
+  // body를 기준으로 위치를 지정합니다.
+
+  // 공을 pageX, pageY 좌표 중앙에 위치하게 합니다.
+  function moveTo(pageX, pageY) {
+    memo.style.left = pageX - shiftX + "px";
+    memo.style.top = pageY - shiftY + "px";
+  }
+
+  // 포인터 아래로 공을 이동시킵니다.
+  moveTo(event.pageX, event.pageY);
+
+  function onMouseMove(event) {
+    moveTo(event.pageX, event.pageY);
+  }
+
+  // (2) mousemove로 공을 움직입니다.
+  document.addEventListener("mousemove", onMouseMove);
+
+  // (3) 공을 드롭하고, 불필요한 핸들러를 제거합니다.
+  memo.onmouseup = function () {
+    document.removeEventListener("mousemove", onMouseMove);
+    memo.onmouseup = null;
+  };
+
+  console.log(memo);
+  //   if(event.target)
+  //   let li = event.target.closest('td'); // (1)
+
+  //   if (!td) return; // (2)
+
+  //   if (!table.contains(td)) return; // (3)
+
+  //   highlight(td); // (4)
 }
 
 function render() {
@@ -87,9 +141,15 @@ function render() {
 
 function addMemo() {
   //정보 값들을 객체에 담고, 어레이에 push, 로컬에 문자로 저장.
+  const time = new Date();
+  const year = time.getFullYear();
+  const month = String(time.getMonth() + 1).padStart(2, "0"); // getMonth()는 0부터 시작하므로 1을 더해줍니다.
+  const day = String(time.getDate()).padStart(2, "0");
+
   let memoInfo = {
     memoTitle: title.value,
     memoContent: content.value,
+    date: `${year}/${month}/${day}`, //시간 표기
     leng: memoArray.length,
   };
   memoArray.push(memoInfo);
@@ -104,13 +164,24 @@ function addMemo() {
 function delMemo() {
   //memoArray 안의 모든요소는 memoInfo의 객체들을 요소이다. 즉, len을 참조할 수 있음.
   //event.srcElement.id는 이벤트가 발생한 요소의 id값(여기선 순서가 됨. listrender에서 정해줬음)
-  const idx = memoArray.find((el) => el.leng == event.target.id); //이걸 이용해서 현재 id값과 이벤트(클릭)가 발생한 녀석의 id가 일치하는지 여부
-  if (idx) {
-    const findIdx = memoArray.findIndex((el) => el.leng === idx.leng); //주어진 판별 함수를 만족하는 배열의 첫 번째 요소에 대한 인덱스를 반환, 그 뒤의 연산자는 내가 누른 이벤트와 그 이벤트가 발생한 위치의 len이 같을 경우
-    memoArray.splice(findIdx, 1);
-  }
+
+  const selectedList = event.target.parentNode;
+  selectedList.remove();
+
+  const memoElements = document.querySelectorAll(".memoCard");
+
+  const newMemoArray = [];
+  memoElements.forEach((el, i) => {
+    let newEl = {
+      memoTitle: el.children[2].innerHTML,
+      memoContent: el.children[3].innerHtml,
+      date: el.children[4].innerHTML,
+      leng: i,
+    };
+    newMemoArray.push(newEl);
+  });
+  memoArray = newMemoArray;
   localStorage.setItem("memoArray", JSON.stringify(memoArray));
-  render();
 }
 
 //스크롤 버튼 추가
@@ -119,6 +190,7 @@ const scrollBtn = document.createElement("button");
 scrollBtn.innerHTML = "&darr;";
 scrollBtn.setAttribute("class", "scrollDown");
 document.body.appendChild(scrollBtn);
+
 const scrollDown = document.querySelector(".scrollDown");
 scrollDown.addEventListener("click", function () {
   window.scrollTo({ top: document.body.scrollHeight, behavior: "smooth" });
